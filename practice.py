2df27dc86ac36fac7faad7cc73aabad5deae4e3b
SANGKI-CHEONG <75767164+SANGKI-CHEONG@users.noreply.github.com>
2020-12-11 08:16:35 +0900

    first versiov

---
+++ practice.py
@@ -0,0 +1,136 @@
+print(5) #숫자
+print(100000)
+print(5*3+(2-2)/4)
+
+print("장하다.") #문자
+print('잘하고있다')
+print("넌 할수있다"*3)
+# 한줄주석사용하기
+'''여러줄
+주석
+처리하기
+방법'''
+
+# 들여쓰기
+print('a"')
+print('b')
+print('c')
+print('''
+html
+<li>css</li>
+<li>javascript</li>
+
+
+<ol>
+<li>html</li>
+<li>css</li>
+<li>javascript</li>
+</ol>
+
+<strong>근로소득</strong> 외 <u>다른소득</u>을 만들어라.
+<title>골프치는법</title>
+
+<h1>머리그대로(들고)나가지말고(밀어올려)</h1>
+<h3>머리그대로(들고)나가지말고(밟올려)</h3>
+<h5>왼팔천천히(들고)나가지말고(밀어)</h5>
+<h6>왼팔천천히(밀고)나가지말고(꿀밤)</h6>
+
+<img src="골프01.jpg" width=50%>
+
+<br>근로소득외 다른소득을 만들자.
+<br>let's smile. let's praise. let's treat money very seriously.
+<br>꿈을 크게 갖져라. 좋아하는 일을 하라.<br>"행함"으로 새로워지는 삶을 살아라.
+목표를 시각화하고 혼자말 자주하기.
+<br><br><br>
+<p>근로소득외 다른소득을 만들자.
+let's smile. let's praise. let's treat money very seriously.</p>
+꿈을 크게  가져라. 좋아하는 일을 하라. <p>"행함"으로 새로워지는 삶을 살아라.
+목표를 시각화하고 혼자말 자주하기.</p>
+''')
+
+# 조건 참/거짓
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+
+
+# 변수, 이름규칙(영어(a 대,소문자구분). -, / 숫자(1))-첫글짜불가
+a=10
+b=5
+print(a)
+print(b)
+print(a*b)
+b=7
+print(a*b)
+
+
+print("XX행 열차가 들어오고 있습니다.")
+
+station = "광주행"
+print(station + "열차가 들어오고 있습니다.")
+
+#함수-이름(인자값,인자값,인자갑, ...) ---> 리턴 값
+
+print('hi','hellow')
+input("이름을 압력하세요 ?")
+name=input('이름을 입력하세요?')
+print('안녕하세요?',name)
+
+# 자료형함수 / type()
+# 숫자형 (정수형-int, 실수형-float)
+a=10
+b=5
+c=2.0
+d=0.5
+print(a+b)
+print(a//b)
+print(a%c)
+print(a**d)
+# 문자형 ( str~)
+# 논리형 (<, <=, >, >=, ==, !=, or, and, not )
+
+
+# 문자열형 (str ~, ' ', " " /문자+문지, 문자*정수)
+
+
+# 군집자료형(list, tuple, set, dictionary)
+
+# 자료형 변환
+
+# if
+num=int(input('숫자 하나 입력 : '))
+if num > 0 :
+    print('{} 은 양수입니다')
+
+# if ~ else
+
+# if ~ elif
+
+# if ~ elif ~ else
+
+# while문 / while 조건 > 실행코드 / 조건이 참일때 실행반복
+
+a=int(inout('a'))
+while a>=0 :
+    print('실행')
+
+# 무한루프
+
+# n번 반복하기
+
+# ~까지 반복하기
+
+
+
+
+
+
+
+
+
+
+
+
+
+
